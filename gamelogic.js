a38d5edba9434b9a119d8ff39ddd6808b3b62ac3
William Suh <williamsuh60@gmail.com>
2020-04-22 17:57:17 -0500

    added comments on source code to understand better

--- gamelogic.js
+++ gamelogic.js
@@ -10,16 +10,16 @@ function startGame() {
 }
 
 var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
+    canvas: document.createElement("canvas"),  //creates html element using js
+    start: function() {
         this.canvas.width = 480;
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
-        this.interval = setInterval(updateGameArea, 20);
+        this.interval = setInterval(updateGameArea, 20);    //20 is in milliseconds
         },
-    clear : function() {
+    clear: function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
@@ -46,7 +46,7 @@ function component(width, height, color, x, y, type) {
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
     }
-    this.newPos = function() {
+    this.newPos = function() {                      //new position(changes game piece's location)
         this.gravitySpeed += this.gravity;
         this.x += this.speedX;
         this.y += this.speedY + this.gravitySpeed;
@@ -79,12 +79,12 @@ function component(width, height, color, x, y, type) {
 function updateGameArea() {
     var x, height, gap, minHeight, maxHeight, minGap, maxGap;
     for (i = 0; i < myObstacles.length; i += 1) {
-        if (myGamePiece.crashWith(myObstacles[i])) {
+        if (myGamePiece.crashWith(myObstacles[i])) {           //game over, thats why there's return
             return;
         } 
     }
-    myGameArea.clear();
-    myGameArea.frameNo += 1;
+    myGameArea.clear();                                        //clearing the frame before adding new one
+    myGameArea.frameNo += 1;                                   //keeping track of frame number
     if (myGameArea.frameNo == 1 || everyinterval(150)) {
         x = myGameArea.canvas.width;
         minHeight = 20;
@@ -93,11 +93,11 @@ function updateGameArea() {
         minGap = 50;
         maxGap = 200;
         gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
-        myObstacles.push(new component(10, height, "white", x, 0));
+        myObstacles.push(new component(10, height, "white", x, 0));                             //either top or bottom obstacle part
         myObstacles.push(new component(10, x - height - gap, "white", x, height + gap));
     }
     for (i = 0; i < myObstacles.length; i += 1) {
-        myObstacles[i].x += -1;
+        myObstacles[i].x += -1;                              // change variable amount
         myObstacles[i].update();
     }
     myScore.text="SCORE: " + myGameArea.frameNo;
